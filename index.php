dbb186914baca8daa271bcf61784c0482f8674e9
iv20052@ya.ru <>
2013-08-13 12:37:18 +0000

    aaa

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
      <title>Hello World</title>
   </head>
   <body>
-     <?php echo '<p>Hello World</p>'; ?>
-  </body>
+     <?php echo '<p>Hello World</p><p>This was edited in Codenvy!</p>'; ?>
+     <?php echo('<p>vsfvjskjvffjs</p>');?>
+  </body>iillljj
 </html>
+
